ee23b2129ee9537726cf8f27af41960703db909c
James Marshall <13554810+jmarshall1975@users.noreply.github.com>
2022-04-11 19:42:27 +0000

    Random art

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import turtle
+import random
 
-shelly = turtle.Turtle()
+turtle.bgcolor('black')
+myTurtle = turtle.Turtle()
 
-for n in range(36):
-    for i in range(6):
-        shelly.forward(100)
-        shelly.left(60)
-    shelly.right(10)
+colourList = ['#ff0000','#ff00ff','#00ff00','#0000ff','#ffffff','#ffff00','#00ffff']
+
+for n in range(18):
+  randomColour = random.randint(0, len(colourList) - 1)
+  randomSides = random.randint(3, 8)
+  randomSize = random.randint(50, 200)
+  myTurtle.color(colourList[randomColour])
+  for i in range(randomSides):
+    myTurtle.forward(randomSize)
+    myTurtle.left(360/randomSides)
+  myTurtle.left(20)
